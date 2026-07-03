9f2a35357440e377e0077d682eec1995e85e706e
Ryan Avancena <ravancena.dev@gmail.com>
2026-07-03 13:01:12 -0700

    first

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, League_Gothic, Dela_Gothic_One } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -9,6 +9,18 @@ const geistSans = Geist({
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const leagueGothic = League_Gothic({
+  variable: "--font-league-gothic",
+  weight: "400",
+  subsets: ["latin"],
+});
+
+const delaGothicOne = Dela_Gothic_One({
+  variable: "--font-dela-gothic-one",
+  weight: "400",
   subsets: ["latin"],
 });
 
@@ -25,7 +37,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} ${leagueGothic.variable} ${delaGothicOne.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
